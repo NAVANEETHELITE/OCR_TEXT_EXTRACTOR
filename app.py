dfe04b46bc964e5c4647992417f90777188522e4
NAVANEETH <navaneethanselvakumar@gmail.com>
2021-06-02 15:37:33 -0400

    Initail Commit

---
+++ app.py
@@ -0,0 +1,55 @@
+import datetime
+import io
+import re 
+import pytesseract
+from PIL import Image
+from flask import Flask, request, render_template, redirect, url_for, session
+
+app = Flask(__name__)
+
+# Secret key for sessions encryption
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+
+@app.route('/')
+def home():
+    return render_template("index.html", title="Text Extractor")
+
+
+@app.route('/scanner', methods=['GET', 'POST'])
+def scan_file():
+    if request.method == 'POST':
+        start_time = datetime.datetime.now()
+        image_data = request.files['file'].read()
+
+        scanned_text = pytesseract.image_to_string(Image.open(io.BytesIO(image_data)))
+        scanned_text = re.sub('[^a-zA-Z0-9]', ' ',scanned_text)
+        print("Found data:", scanned_text)
+
+        session['data'] = {
+            "text": scanned_text,
+            "time": str((datetime.datetime.now() - start_time).total_seconds())
+        }
+
+        return redirect(url_for('result'))
+
+
+@app.route('/result')
+def result():
+    if "data" in session:
+        data = session['data']
+        return render_template(
+            "result.html",
+            title="Result",
+            time=data["time"],
+            text=data["text"],
+            words=len(data["text"].split(" "))
+        )
+    else:
+        return "Wrong request method."
+
+
+if __name__ == '__main__':
+    # Setup Tesseract executable path
+    pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+    app.run(debug=True)
